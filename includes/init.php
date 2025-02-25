824f5aa849fa834e33c167b048eac2d9e3900a5a
Dominic Tremblay <dominictremblay24@gmail.com>
2025-02-25 08:59:32 -0500

    ajouter commentaires init

--- includes/init.php
+++ includes/init.php
@@ -1,2 +1,33 @@
 <?php
-// Gère la logique d'initialisation et de filtrage des produits.
+/**
+ *  Gère la logique d'initialisation et de filtrage des produits.
+ * 
+ */
+
+// Charger le fichier produits.json
+
+// si msg erreur => $config["message_erreur"]
+
+// Obtenir le parametre categorie
+
+// Generer les options
+
+// filtrer les produits en fonction du parametre categorie
+
+// Generer la table html
+
+/**
+ * Creer une date litterale
+ * 
+ */
+
+ /**
+ * Validation de formulaire
+ * 
+ */
+
+ // valider le formulaire si action=ajouter-produit
+
+ // obtenir les parametres nom, prix, categorie
+
+ // valider le formulaire et gerer les erreurs ou succes.
